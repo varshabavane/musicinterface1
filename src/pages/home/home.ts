064fd2027c70e751ca4f01ab3ce64121b760eb37
varsha bavane <varshab13bavane@gmail.com>
2018-04-12 11:04:19 +0530

    media imported and imgs added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,43 +1,66 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
+import { Media, MediaObject } from "@ionic-native/media";
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
+  music: MediaObject;
   tracks: any;
   playing: boolean = true;
   currentTrack: any;
   progressInterval: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private media: Media) {
     this.tracks = [
-      { title: "main rang sharbatonka", artist: "shahid", playing: 'assets\songs\ppnh.mp3', progress: 0 },
-      { title: "phone me teri photo", artist: "neha kakkar", playing:'assets\songs\Pmtp.mp3' , progress: 0 },
+      {
+        title: "main rang sharbatonka",
+        artist: "shahid",
+        playing: false,
+        progress: 0,
+        songPath:'/android_asset/www/assets/songs/ppnh.mp3',
+        img:'/android_asset/www/assets/imgs/deepika.jpg'
+      },
+      {
+        title: "phone me teri photo",
+        artist: "neha kakkar",
+        playing: false,
+        progress: 0,
+        songPath:'/android_asset/www/assets/songs/pmtp.mp3',
+        img:'/android_asset/www/assets/imgs/deepika.jpg'
+      },
       {
         title: "dhaga dhaga",
         artist: "pooja sawant",
-        playing:'assets\songs\dhagadhaga.mp3',
-        progress: 0
+        playing: false,
+        progress: 0,
+        songPath:'/android_asset/www/assets/songs/dhagadhaga.mp3',
+        img:'/android_asset/www/assets/imgs/deepika.jpg'
       },
       {
         title: "Tere sang yara",
         artist: "akshay",
-        playing:'assets\songs\tere sang Yaara.mp3',
-        progress: 0
+        playing: false,
+        progress: 0,
+        songPath:'/android_asset/www/assets/songs/rainMashup.mp3',
+        img:'/android_asset/www/assets/imgs/1.jpg'
       },
       {
         title: "rain Mashup",
         artist: "neha kakar",
-        playing:'assets\songs\rainMashup.mp3',
-        progress: 0
+        playing: false,
+        progress: 0,
+        songPath:'/android_asset/www/assets/songs/Tere Sang Yaara - 320Kbps.mp3',
+        img:'/android_asset/www/assets/imgs/ranbir.jpg'
       }
     ];
     this.currentTrack = this.tracks[0];
   }
 
   playTrack(track) {
+    //this.music=this.media.create(this.currentTrack)
     for (let checkTrack of this.tracks) {
       if (checkTrack.playing) {
         this.pauseTrack(checkTrack);
@@ -45,6 +68,8 @@ export class HomePage {
     }
     track.playing = true;
     this.currentTrack = track;
+    this.music=this.media.create(this.currentTrack.songPath);
+    this.music.play();
     this.progressInterval = setInterval(() => {
       track.progress < 100 ? track.progress++ : (track.progress = 0);
     }, 10000);
@@ -52,6 +77,7 @@ export class HomePage {
 
   pauseTrack(track) {
     track.playing = false;
+    this.music.pause();
     clearInterval(this.progressInterval);
   }
 
